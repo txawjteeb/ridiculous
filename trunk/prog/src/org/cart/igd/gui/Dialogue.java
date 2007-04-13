cf09ab6c99940020068b157314fde7436d50ab60
cameron.khanzadian <cameron.khanzadian@ecb28c3a-042b-0410-81be-cf60340d36b0>
2007-04-13 22:11:53 +0000

    Added dialogue effects, not completed. Updated the font to 20th century.

--- trunk/prog/src/org/cart/igd/gui/Dialogue.java
+++ trunk/prog/src/org/cart/igd/gui/Dialogue.java
@@ -13,8 +13,9 @@ public class Dialogue extends GUI {
 	static ArrayList <DialogueInfo> renderDialogue = new ArrayList <DialogueInfo>();
 //	private UserInput input;
 	private UserInput input;
-	Texture[] animalIcons = new Texture[10];
-	Texture dialogueBackground;
+	private Texture[] animalIcons = new Texture[10];
+	private Texture dialogueBackground;
+	private Texture border;
 	
 	/**
 	 * param1 informative string
@@ -27,6 +28,7 @@ public class Dialogue extends GUI {
 	 * to allow for game state change and gui state change
 	 * @param GameState refference to container
 	 **/
+	 
 	public Dialogue(GameState gameState){
 		super(gameState);//superclass GUI contain ref to GameState
 		input = Kernel.userInput;
@@ -43,6 +45,7 @@ public class Dialogue extends GUI {
 			animalIcons[i] = Kernel.display.getRenderer().loadImage("img/dialogue/" + i + ".png");
 		}
 		dialogueBackground = Kernel.display.getRenderer().loadImage("img/dialogue/background.png");
+		border = Kernel.display.getRenderer().loadImage("img/dialogue/border.png");
 	}
 	
 	boolean t = true;
@@ -63,8 +66,8 @@ public class Dialogue extends GUI {
 			g.drawImageAlpha(dialogueBackground,0,400,1f);
 			for(int i = 0;i<renderDialogue.size();i++){
 				DialogueInfo di = renderDialogue.get(i);
-				g.drawImageAlpha(animalIcons[di.animal],1,500-i*100,1f);
-				g.drawBitmapString(di.words,70,500-i*100);
+				di.update();
+				di.draw(g);
 			}
 		g.glgEnd();
 	}
@@ -74,103 +77,163 @@ public class Dialogue extends GUI {
 	}
 	
 	private class DialogueInfo{
-		int id,animal,type;
+		int id,animal,type,x,y;
 		String words;
-		public DialogueInfo(int id, int animal, int type, String words){
+		boolean mouseover = false;
+		
+		int degreesLeft=0;
+		int degreesRight=0;
+		int degreesCurrent=0;
+	//	int degreeProgress[] = new int[]{50,30,20,15,12,9,7,5,3,2,1,0};
+		int degreeProgress[] = new int[]{50,40,30,20,10,5};
+		int progress=0;
+		boolean swinging = false;
+		boolean left = false;
+		
+	//	public DialogueInfo(int id, int animal, int type, String words, int x, int y){
+	//		this.id = id;
+	//		this.animal = animal;
+	//		this.type = type;
+	//		this.words = words;
+	//		this.x = x;
+	//		this.y = y;
+	//	}
+		
+		public DialogueInfo(int id, int animal, int type, String words, int place){
 			this.id = id;
 			this.animal = animal;
 			this.type = type;
 			this.words = words;
-		} 
+			switch(place){
+				case 0:
+					x = 200;
+					y = 600;
+				break;
+				case 1:
+					x = 270;
+					y = 520;
+				break;
+				case 2:
+					x = 270;
+					y = 440;
+				break;
+				case 3:
+					x = 270;
+					y = 360;
+				break;
+				case 4:
+					x = 270;
+					y = 280;
+				break;
+			}
+		}
+		
+		public void update(){
+			mouseover = false;
+			if(input.mousePos[0]>x &&input.mousePos[0]<x+64&&input.mousePos[1]>y&&input.mousePos[1]<y+64){
+				swinging = true;
+				left = true;
+				progress = 0;
+				degreesCurrent = 0;
+				degreesLeft = degreeProgress[progress];
+			}
+			//mouseover = false;
+		//	if(input.mousePos[0]>x &&input.mousePos[0]<x+64&&input.mousePos[1]>y&&input.mousePos[1]<y+64) mouseover = true;
+		}
+		
+		
+		public void draw(GLGraphics g){
+			switch(type){
+				case 0:
+				break;
+				case 1:
+					if(swinging){
+						
+						if(left){
+							degreesCurrent+=20;
+							g.drawImageRotate(animalIcons[animal],x,y,degreesCurrent);
+							g.drawImageRotate(border,x,y,degreesCurrent);
+							if(degreesCurrent>=degreesLeft){
+								left = false;
+								degreesRight = -degreeProgress[progress];
+							}
+						} else {
+							degreesCurrent-=20;
+							g.drawImageRotate(animalIcons[animal],x,y,degreesCurrent);
+							g.drawImageRotate(border,x,y,degreesCurrent);
+							if(degreesCurrent<=degreesRight){
+								left = true;
+								progress++;
+								
+								degreesLeft = degreeProgress[progress];
+							}
+						}
+						
+						if(progress==degreeProgress.length-1){
+							swinging = false;
+						}
+					} else{
+						g.drawImageAlpha(animalIcons[animal],x,y,1f);
+						g.drawImageAlpha(border,x,y,1f);
+					}
+					g.drawBitmapString(words,x+76,y+20);
+					/*if(mouseover){
+						g.drawImageRotate(animalIcons[animal],x,y,6);
+						g.drawImageRotate(border,x,y,6);
+					} else{
+						g.drawImageAlpha(animalIcons[animal],x,y,1f);
+						g.drawImageAlpha(border,x,y,1f);
+					}
+					g.drawBitmapString(words,x+76,y+20);	
+					*/
+				break;
+				case 2:
+				break;
+			}
+		}
 	}
-	/*
-	private class ActiveDialogue extends Thread{
-		int lastMousePress[] = new int[]{0,0};
-	}
-	*/
-//	private class ActiveDialogue {
-//		int lastMousePress[] = new int[]{0,0};
-//	}
 
 	private class ActiveDialogue extends Thread{
 		int lastMousePress[] = new int[]{0,0};
-		//Dialogue dialogue;
-		/*
+
 		public void run(){
-				dialogue.renderDialogue.add(new DialogueInfo(0,0,1,"Oh hey flamingo... uhgg"));
-				dialogue.renderDialogue.add(new DialogueInfo(1,1,1,"I escaped, and you need to too"));
-				dialogue.renderDialogue.add(new DialogueInfo(2,1,1,"Hey Giraffe, ill be right back"));
-				dialogue.renderDialogue.add(new DialogueInfo(3,1,1,"Is something wrong?"));
-				switch(getSelection()){
-					case 1:
-						dialogue.clearDialogue();
-						dialogue.renderDialogue.add(new DialogueInfo(4,0,1,"Alright, these trees are to low for me to eat off of. It makes my neck hurt, I could use a vacation."));
-						pause(5000);
-						break;
-					case 2:
-						dialogue.clearDialogue();
-						break;
-					case 3:
-						dialogue.clearDialogue();
-						dialogue.renderDialogue.add(new DialogueInfo(5,0,1,"Its just my neck. What are you doing out of your cage?"));
-						dialogue.renderDialogue.add(new DialogueInfo(6,1,1,"Oh yea, it doesn't look so good. You need to get yourself out of this cage"));
-						dialogue.renderDialogue.add(new DialogueInfo(7,1,1,"I can tell you are in pain, but no time for that, you have to escape and save your life"));
-						switch(getSelection()){
-							case 6:
-								dialogue.clearDialogue();
-								dialogue.renderDialogue.add(new DialogueInfo(8,0,1,"You don't look so good yourself. Well until my neck feels better, I'm not leaving."));
-								pause(5000);
+			
+					Dialogue.renderDialogue.add(new DialogueInfo(0,1,1,"Oh hey flamingo... uhgg",0));
+					Dialogue.renderDialogue.add(new DialogueInfo(1,0,1,"I escaped, and you need to too",1));
+					Dialogue.renderDialogue.add(new DialogueInfo(2,0,1,"Hey Giraffe, ill be right back",2));
+					Dialogue.renderDialogue.add(new DialogueInfo(3,0,1,"Is something wrong?",3));
+					switch(getSelection()){
+						case 1:
+							Dialogue.clearDialogue();
+							Dialogue.renderDialogue.add(new DialogueInfo(4,1,1,"Alright, these trees are to low for me to eat off of. It makes my neck hurt, I could use a vacation.",0));
+							pause(5000);
 							break;
-							case 7:
-								dialogue.clearDialogue();
-								dialogue.renderDialogue.add(new DialogueInfo(9,0,1,"Yea, of course there is no time for my pain. You are just like the zookeepers."));
-								pause(5000);
+						case 2:
+							Dialogue.clearDialogue();
 							break;
-						}
-						break;
-				}
-				
-						
-		}
-		*/
-	public void run(){
-		
-				Dialogue.renderDialogue.add(new DialogueInfo(0,0,1,"Oh hey flamingo... uhgg"));
-				Dialogue.renderDialogue.add(new DialogueInfo(1,1,1,"I escaped, and you need to too"));
-				Dialogue.renderDialogue.add(new DialogueInfo(2,1,1,"Hey Giraffe, ill be right back"));
-				Dialogue.renderDialogue.add(new DialogueInfo(3,1,1,"Is something wrong?"));
-				switch(getSelection()){
-					case 1:
-						Dialogue.clearDialogue();
-						Dialogue.renderDialogue.add(new DialogueInfo(4,0,1,"Alright, these trees are to low for me to eat off of. It makes my neck hurt, I could use a vacation."));
-						pause(5000);
-						break;
-					case 2:
-						Dialogue.clearDialogue();
-						break;
-					case 3:
-						Dialogue.clearDialogue();
-						Dialogue.renderDialogue.add(new DialogueInfo(5,0,1,"Its just my neck. What are you doing out of your cage?"));
-						Dialogue.renderDialogue.add(new DialogueInfo(6,1,1,"Oh yea, it doesn't look so good. You need to get yourself out of this cage"));
-						Dialogue.renderDialogue.add(new DialogueInfo(7,1,1,"I can tell you are in pain, but no time for that, you have to escape and save your life"));
-						switch(getSelection()){
-							case 6:
-								Dialogue.clearDialogue();
-								Dialogue.renderDialogue.add(new DialogueInfo(8,0,1,"You don't look so good yourself. Well until my neck feels better, I'm not leaving."));
-								pause(5000);
+						case 3:
+							Dialogue.clearDialogue();
+							Dialogue.renderDialogue.add(new DialogueInfo(5,1,1,"Its just my neck. What are you doing out of your cage?",0));
+							Dialogue.renderDialogue.add(new DialogueInfo(6,0,1,"Oh yea, it doesn't look so good. You need to get yourself out of this cage",1));
+							Dialogue.renderDialogue.add(new DialogueInfo(7,0,1,"I can tell you are in pain, but no time for that, you have to escape and save your life",2));
+							switch(getSelection()){
+								case 6:
+									Dialogue.clearDialogue();
+									Dialogue.renderDialogue.add(new DialogueInfo(8,1,1,"You don't look so good yourself. Well until my neck feels better, I'm not leaving.",0));
+									pause(5000);
+								break;
+								case 7:
+									Dialogue.clearDialogue();
+									Dialogue.renderDialogue.add(new DialogueInfo(9,1,1,"Yea, of course there is no time for my pain. You are just like the zookeepers.",0));
+									pause(5000);
+								break;
+							}
 							break;
-							case 7:
-								Dialogue.clearDialogue();
-								Dialogue.renderDialogue.add(new DialogueInfo(9,0,1,"Yea, of course there is no time for my pain. You are just like the zookeepers."));
-								pause(5000);
-							break;
-						}
-						break;
-				}
-				
-				
-						
-		}
+					}
+					
+					
+							
+			}
 	//	public void ActiveDialogue(Dialogue dialogue){
 	//		this.dialogue = dialogue;
 	//	}
