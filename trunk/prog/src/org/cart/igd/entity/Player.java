346f83ac5321aeed45b49f3c0382998c3bc230fb
cameron.khanzadian <cameron.khanzadian@ecb28c3a-042b-0410-81be-cf60340d36b0>
2007-04-24 21:02:02 +0000

    Added Animal Class, updated entities and item classes

--- trunk/prog/src/org/cart/igd/entity/Player.java
+++ trunk/prog/src/org/cart/igd/entity/Player.java
@@ -3,14 +3,14 @@ package org.cart.igd.entity;
 import org.cart.igd.math.Vector3f;
 import org.cart.igd.models.obj.OBJModel;
 
-public class Animal extends Entity
+public class Player extends Entity
 {
 	float walkNoiseRadius;
 	float runNoiseRadius;
 	float viewDistance;
 	float hearDistance;//show pings of movement on "radar"
 	
-	public Animal(Vector3f pos, float fD, float bsr,OBJModel model){
+	public Player(Vector3f pos, float fD, float bsr,OBJModel model){
 		super(pos,fD,bsr,model);
 	}
 }
