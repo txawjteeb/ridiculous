14e4e3598e72d050d24f3587acab3dd51a48074a
vitaly.d.maximov <vitaly.d.maximov@ecb28c3a-042b-0410-81be-cf60340d36b0>
2007-05-09 16:55:07 +0000

    Commented some stuff.

--- trunk/prog/src/org/cart/igd/entity/Guard.java
+++ trunk/prog/src/org/cart/igd/entity/Guard.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 
 import org.cart.igd.collision.Collision;
 import org.cart.igd.discreet.Model;
+import org.cart.igd.game.Animal;
+import org.cart.igd.game.Inventory;
 import org.cart.igd.math.Vector3f;
 import org.cart.igd.models.obj.OBJModel;
 import org.cart.igd.states.InGameState;
 
+
+/**
+ * Represents guard's basic functionality 
+ */
 public class Guard extends Entity
 {
 	final float toDeg =57.29578f;
@@ -20,20 +26,25 @@ public class Guard extends Entity
 	
 	/** alaways know loaction on player*/
 	protected Entity player;
+	
 	/** object to face toward when calling changeDirection()*/
 	protected Entity target;
+	
 	/** used by stationary guards to return to post */
 	protected GuardFlag home;
 	
 	protected int currentTarget = 0;
+	
 	protected float posRange = .4f;//margin of error
 	
 	protected float visionDistance = 7f;
+	
 	protected float hearingDistance = 30f;
 	
 	public float refAngleRad = 0f;
 	
 	public boolean hearNoise = false;
+	
 	public boolean investigating = false;
 	
 	/**
@@ -44,7 +55,7 @@ public class Guard extends Entity
 	 * @param OBJModel model: .obj format file data
 	 * @param InGameState refference the game
 	 * @param float scale of the model
-	 **/
+	 */
 	public Guard(Vector3f pos, float fD, float bsr, OBJModel model, 
 			InGameState igs,float speed)
 	{
@@ -64,7 +75,7 @@ public class Guard extends Entity
 	 * @param Model model: .3ds format file data
 	 * @param InGameState refference the game
 	 * @param float scale of the model
-	 **/
+	 */
 	public Guard(Vector3f pos, float fD, float bsr, Model model, 
 			InGameState igs,float speed)
 	{
@@ -75,6 +86,10 @@ public class Guard extends Entity
 		this.speed = speed;
 	}
 	
+	/**
+	 * @return new Vector3f relative to previos position based on 
+	 * degree direction and distance
+	 */
 	public Vector3f getNewPointDeg(float deg, float distance){
 		float newPosX = position.x + ( distance*(float)Math.cos(deg * toRad) );
 		float newPosZ = position.z + ( distance*(float)Math.sin(deg * toRad) );
@@ -82,6 +97,10 @@ public class Guard extends Entity
 		return new Vector3f(newPosX,position.y,newPosZ);
 	}
 	
+	/**
+	 * @return new Vector3f relative to previos position based on 
+	 * radian direction and distance
+	 */
 	public Vector3f getNewPointRad(float rad, float distance){
 		float newPosX = position.x + ( distance * (float)Math.cos(rad) );
 		float newPosZ = position.z + ( distance * (float)Math.sin(rad) );
@@ -91,6 +110,26 @@ public class Guard extends Entity
 	
 	public void update(long elapsedTime){
 		super.update(elapsedTime);
+	}
+	
+	/**
+	 * check if the guard is close enough to catch the player to be 
+	 * returned to the cage
+	 **/
+	public void capturePlayer(){
+		if(Collision.stsXZ(position,1f,igs.player.position,
+				igs.player.boundingSphereRadius)){
+			 igs.guardSquad.reset();
+			 igs.player.position = new Vector3f(-20f,0f,-20f);
+			 igs.removePartyAnimals();
+			 Animal a = igs.getAnimal("Turtles");
+			 if(a!=null){
+			 	if( a.state!=Inventory.SAVED_IN_PARTY
+			 		&& a.state!=Inventory.SAVED_IN_BUSH){
+			 		igs.inventory.PSYCH_CAUGHT_BEFORE_FREEING_TURTLES = 1;
+			 	}
+			 }
+		}
 	}
 	
 	/** adjust yrotation to position of the target */
--- trunk/prog/src/org/cart/igd/entity/WalkingGuard.java
+++ trunk/prog/src/org/cart/igd/entity/WalkingGuard.java
@@ -10,6 +10,12 @@ import org.cart.igd.models.obj.OBJModel;
 import org.cart.igd.states.InGameState;
 import org.cart.igd.game.*;
 
+/**
+ * A more specific implementation of guard class,
+ * Patrols a given set of pints in a continuous cycle,
+ * When sighting target pursu untill capture or loss of sight
+ * Resumes patrol cycle when no targets in sight.
+ */
 public class WalkingGuard extends Guard
 {	
 	/**
@@ -20,7 +26,7 @@ public class WalkingGuard extends Guard
 	 * @param OBJModel model: .obj format file data
 	 * @param InGameState refference the game
 	 * @param float scale of the model
-	 **/
+	 */
 	public WalkingGuard(Vector3f pos, float fD, float bsr, OBJModel model, 
 			InGameState igs,float speed)
 	{
@@ -35,13 +41,17 @@ public class WalkingGuard extends Guard
 	 * @param float fD: direction entity is facing ( y rotation )
 	 * @param float bsr: bounding sphere radius used for collision detection
 	 * @param Model model: .3ds format file data
-	 **/
+	 */
 	public WalkingGuard(Vector3f pos, float fD, float bsr, Model model, 
 			InGameState igs, float speed )
 	{
 		super(pos,fD,bsr,model,igs,speed);
 	}
 	
+	/**
+	 * cycle through guards AI cycle checking for specific targets
+	 * @param long elapsedTime for fps/time sensitive functions
+	 */
 	public void update(long elapsedTime)
 	{
 		super.update(elapsedTime);
@@ -62,27 +72,18 @@ public class WalkingGuard extends Guard
 			}
 		}
 		
-		
-		
 		changeDirection();
 		walkForward(elapsedTime);
 		
 		Renderer.info[6] = "walking guard: "+position.x+"/"+position.z;
-		if(Collision.stsXZ(position,1f,igs.player.position,igs.player.boundingSphereRadius)){
-			 igs.guardSquad.reset();
-			 igs.player.position = new Vector3f(-20f,0f,-20f);
-			 igs.removePartyAnimals();
-			 Animal a = igs.getAnimal("Turtles");
-			 if(a!=null){
-			 	if(a.state!=Inventory.SAVED_IN_PARTY&&a.state!=Inventory.SAVED_IN_BUSH){
-			 		igs.inventory.PSYCH_CAUGHT_BEFORE_FREEING_TURTLES = 1;
-			 	}
-			 }
-		}
-
 		
+		capturePlayer();
 	}
 	
+	/**
+	 * detect Player entity in a circlular area offset forward by the radius
+	 * of the circle in front of the guard 
+	 */
 	public void lookForPlayer()
 	{
 		Vector3f s = getNewPointDeg(facingDirection, visionDistance);
@@ -97,6 +98,9 @@ public class WalkingGuard extends Guard
 		}
 	}
 	
+	/**
+	 * Detect Noise entities in a circular area with radius of hearingDistance
+	 */
 	public void listenForNoise()
 	{
 		Renderer.info[8]= "walking guard: no noise";
@@ -117,6 +121,9 @@ public class WalkingGuard extends Guard
 		}
 	}
 	
+	/**
+	 * Cycle through a path when failed tetecting either Player or Noise  
+	 */
 	private void followPath()
 	{
 		if(currentTarget < path.size()-1)
