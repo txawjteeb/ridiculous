1953ae57c0144e6a36b63d03e2aa38f892318706
spencer.t.allen <spencer.t.allen@ecb28c3a-042b-0410-81be-cf60340d36b0>
2007-05-11 17:33:13 +0000

    Comments keep coming.

--- trunk/prog/src/org/cart/igd/util/WinDataInputStream.java
+++ trunk/prog/src/org/cart/igd/util/WinDataInputStream.java
@@ -5,13 +5,30 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * WinDataInputStream.java
+ *
+ * General Function: An extension of DataInputStream to gather Windows safe data.
+ */
 public class WinDataInputStream extends DataInputStream
 {
+	/**
+	 * Constructor
+	 *
+	 * General Function: Creates an instance of WinDataInputStream.
+	 *
+	 * @param in The InputStream to read from.
+	 */
 	public WinDataInputStream(InputStream in)
 	{
 		super(in);
 	}
 	
+	/**
+	 * readNullTerminatedString
+	 *
+	 * General Function: Reads a null terminated String from the InputStream.
+	 */
 	public String readNullTerminatedString() throws IOException
 	{
 		StringBuffer stringBuffer = new StringBuffer();
@@ -27,6 +44,13 @@ public class WinDataInputStream extends DataInputStream
 		return stringBuffer.toString().trim();
 	}
 	
+	/**
+	 * readString
+	 *
+	 * General Function: Returns a String of char values from InputStream.
+	 *
+	 * @param numOfChars The number of char values to read into the String.
+	 */
 	public String readString(int numOfChars) throws IOException
 	{
 		StringBuffer stringBuffer = new StringBuffer();
@@ -41,6 +65,11 @@ public class WinDataInputStream extends DataInputStream
 		return stringBuffer.toString().trim();
 	}
 	
+	/**
+	 * readWinFloat
+	 *
+	 * General Function: Returns a Windows valid float from InputStream.
+	 */
 	public float readWinFloat() throws IOException
 	{
 		byte[] buffer = new byte[4];
@@ -62,6 +91,11 @@ public class WinDataInputStream extends DataInputStream
 		return (new DataInputStream(new ByteArrayInputStream(buffer)).readFloat());
 	}
 	
+	/**
+	 * readWinInt
+	 *
+	 * General Function: Returns a Windows valid integer from InputStream.
+	 */
 	public int readWinInt() throws IOException
 	{
 		byte[] buffer = new byte[4];
@@ -69,6 +103,11 @@ public class WinDataInputStream extends DataInputStream
 		return (((buffer[3] & 0xff) << 24) | ((buffer[2] & 0xff) << 16) | ((buffer[1] & 0xff) << 8) | (buffer[0] & 0xff));
 	}
 	
+	/**
+	 * readWinUnsignedShort
+	 *
+	 * General Function: Returns a Windows valid unsigned short from InputStream.
+	 */
 	public int readWinUnsignedShort() throws IOException
 	{
 		byte[] buffer = new byte[2];
